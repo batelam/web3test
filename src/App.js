ff50b55b3e120c78e6207fddbc3893eaf551e6d4
batelam@post.jce.ac.il <batelam>
2023-01-17 13:53:14 +0200

    Connecting React to Metamask to Ganache

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
+import {useState, useEffect} from 'react';
+import detectEthereumProvider from '@metamask/detect-provider';
 
 function App() {
+
+  const [web3Api, setWeb3Api] = useState({
+    provider:null,
+    web3:null,
+    contract:null
+  })
+
+  useEffect(()=>{
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider()
+      if(provider){
+        setWeb3Api(
+          {
+            provider:provider,
+            web3: new web3(provider),
+            contract:null
+
+          }
+        )
+      } else{
+        console.log("Please install MetaMask")
+      }
+    }
+    loadProvider()
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={async () =>{
+          const accounts = await window.ethereum.request({method:"eth_requestAccounts"})
+          console.log(accounts)
+        } }
+      >Connect to MetaMask</button>
     </div>
   );
 }
