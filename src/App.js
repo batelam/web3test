dc610ef9a221e3a425304dd7a7826d852e852be4
batelam@post.jce.ac.il <batelam>
2023-01-24 13:52:30 +0200

    Contract functions on React

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Web3 from 'web3';
 import {useState, useEffect} from 'react';
 import detectEthereumProvider from '@metamask/detect-provider';
+import { loadContract } from './utils/load-contract';
 
 function App() {
 
@@ -11,15 +12,20 @@ function App() {
     contract:null
   })
 
+
+  const [balance, setBalance] = useState(null)
+  const [account, setAccount] = useState(null)
+
   useEffect(()=>{
     const loadProvider = async () => {
       const provider = await detectEthereumProvider()
+      const contract = await loadContract("Simplebank", provider)
       if(provider){
         setWeb3Api(
           {
             provider:provider,
-            web3: new web3(provider),
-            contract:null
+            web3: new Web3(provider),
+            contract:contract
 
           }
         )
@@ -30,14 +36,46 @@ function App() {
     loadProvider()
   },[])
 
+  useEffect(
+    () =>{
+      const loadBalance = async () => {
+        const {contract, web3} = web3Api
+        const balance = await web3.eth.getBalance(contract.address)
+        setBalance(web3.utils.fromWei(balance),"ether")
+      }
+      loadBalance()
+    },[web3Api])
+
+  useEffect(() => {
+      const getAccount = async () => {
+        const accounts = await web3Api.web3.eth.getAccounts()
+        setAccount(accounts[0])
+      }  
+      getAccount()
+    },[web3Api.web3])
+
+    const addFunds = async () => {
+      const {contract,web3} = web3Api
+      await contract.addFunds({
+        from:account,
+        value:web3.utils.toWei("1","ether")
+      })
+    }
+
+    const withdraw = async () => {
+      const {contract,web3} = web3Api
+      const withdrawAmount = web3.utils.toWei("0.1","ether")
+      await contract.withdraw(withdrawAmount, {From:account})
+    }
+
   return (
     <div className="App">
-      <button
-        onClick={async () =>{
-          const accounts = await window.ethereum.request({method:"eth_requestAccounts"})
-          console.log(accounts)
-        } }
-      >Connect to MetaMask</button>
+      <div>Current Balance is {balance} Ether</div>     
+      <div>Check that your account is {account}</div>
+      <div>
+        <button onClick={addFunds}>Add 1 ether</button>
+        <button onClick={withdraw}>Withdraw 0.1 ether</button>
+      </div>
     </div>
   );
 }
